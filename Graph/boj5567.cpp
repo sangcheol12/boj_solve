43d4cb393ee29edafeb074c875079c2b56f32385
박 상철 <eric2057@daum.net>
2021-05-15 08:13:00 +0000

    .

--- Graph/boj5567.cpp
+++ Graph/boj5567.cpp
@@ -30,4 +30,52 @@ int main() {
 			cnt++;
 	cout << cnt << '\n';
 }
-//결혼식
+/*결혼식
+풀이법 : 인접 행렬
+*/
+
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+vector<int> G[501];
+int visited[501];
+
+void bfs(int st) {
+	queue<int> q;
+	q.push(st);
+	visited[st] = 1;
+	while(!q.empty()) {
+		int cur = q.front();
+		q.pop();
+		for(int nxt : G[cur]) {
+			if(!visited[nxt]) {
+				q.push(nxt);
+				visited[nxt] = visited[cur]+1;
+			}
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n,m,cnt=0;
+	cin >> n >> m;
+	for(int i=0; i<m; i++) {
+		int u,v;
+		cin >> u >> v;
+		u--, v--;
+		G[u].push_back(v);
+		G[v].push_back(u);
+	}
+	bfs(0);
+	for(int i=0; i<n; i++)
+		if(visited[i]==2 || visited[i]==3)
+			cnt++;
+	cout << cnt << '\n';
+}
+/*결혼식
+풀이법 : 인접 리스트(BFS)
+*/
